b02895bbf08d4456ebc787b19fcff20f49f392af
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2024-12-27 22:40:37 -0800

    fixed color sorting

--- include/intake.h
+++ include/intake.h
@@ -3,10 +3,16 @@
 #include "vex.h"
 
 enum RingColor {
-    ALL_RINGS,
-    RED_RINGS,
-    BLUE_RINGS,
-    NO_RINGS,
+    NO_RINGS = 0b00,
+    RED_RINGS = 0b01,
+    BLUE_RINGS = 0b10,
+};
+
+enum class IntakeState {
+    INPUT = 0,
+    EJECTING = 1,
+    UNJAMMING = 2,
+    CALIBRATING = 3
 };
 
 struct HueWindow {
@@ -22,66 +28,65 @@ public:
     Intake(Intake&&) = default;
     Intake(motor* intake_motor, optical* color_sensor, digital_out* intake_piston);
 
-    void set_filter_mode(RingColor ring_color);
+    void set_filter_mode(RingColor exclude_color);
+    void set_filter_mode_from_current_ring();
 
-    void update();
     void set_velocity(float velocity);
+    void set_raised(bool is_raised);
+    void update();
     void raise();
     void lower();
-    void set_raised(bool is_raised);
-
     void clear_ring();
 
-    void on_jam(void (*callback)());
-
-    void set_filter_mode_current();
-
-    bool has_ring_at_top() const;
     RingColor get_current_ring_color() const;
-    bool has_ring_going_up() const;
-
+    
+    bool has_ring() const;
     bool is_spinning() const;
 
     void pickup();
     void intake();
     void outtake();
     void off();
+    void calibrate();
+
+    float get_ring_position();
 
     RingColor *filter_mode;
     motor* intake_motor;
     optical* color_sensor;
     digital_out* intake_piston;
-
 private:
+    float get_position();
+
     // reading colors
-    void update_color_detections();
     RingColor detected_color() const;
     bool sees_red() const;
     bool sees_blue() const;
 
-    void (*on_jam_callback)();
-
+    IntakeState state = IntakeState::INPUT;
     int direction = 0;
-    unsigned int time_to_stop_reversing = 0;
-    float rotation_when_ring_lost = 0;
-    float rotation_to_eject = 0;
+    float velocity_error_buildup = 0;
+    float previous_ring_position = 0;
     float *target_velocity = 0;
+
     bool detected_ring = false;
-    bool ring_going_up = false;
     bool ejection_pending = false;
+    //bool is_unjamming = false;
     RingColor current_ring_color = NO_RINGS;
 
-    // used to ensure confidence in detection with a moving average
-    float detection_confidence = 0;
-    float red_confidence = 0;
-    float blue_confidence = 0;
-
     // constants
-    // TODO: tune these
-    HueWindow red_hue{5.f, 30.f};
-    HueWindow blue_hue{215.f, 230.f};
-    float ejection_revs = 1; 
-    float new_reading_weight = 1;
-    float confidence_threshold = 0.7;
-    float avg_current = 0;
+    HueWindow red_hue{5.f, 90.f};
+    HueWindow blue_hue{150.f, 230.f};
+
+    const float eject_position_ref = 29.5; // TODO: tune this
+    const float detect_position_ref = 18.5; // TODO: tune this
+    const float top_position_ref = 34;
+    const float links_per_hook = 21;
+    const float links_per_rev = 12;
+    const float calibration_velocity_pct = 4.f;
+
+    unsigned int reverse_end_time = 0;
+    float bottom_position = 0;
+    float score_position = 0;
+    float eject_position = 0;
 };
--- src/intake.cpp
+++ src/intake.cpp
@@ -11,110 +11,114 @@ Intake::Intake(motor* intake_motor, optical* color_sensor, digital_out* intake_p
         color_sensor(color_sensor),
         intake_piston(intake_piston),
         target_velocity(new float (0.0)),
-        filter_mode(new RingColor (ALL_RINGS)) {
-    std::cout << "intake initialized" << std::endl;
-}
+        filter_mode(new RingColor (NO_RINGS)) {}
 
 void Intake::update() {
     // detect rings
     this->color_sensor->setLightPower(100);
     this->color_sensor->setLight(ledState::on);
-    this->update_color_detections();
+    //this->update_color_detections();
     auto color_detected = this->detected_color();
     bool detecting_ring = color_detected != NO_RINGS;
 
-    this->avg_current = this->avg_current * 0.95 + this->intake_motor->current() * 0.05;
-    /*if ((vex::timer::system() / 10) % 10 == 1)
-        std::cout << "intake current = " << avg_current << std::endl;*/
+    auto position = this->get_position();
+    this->direction = *this->target_velocity > 0 ? 1 : *this->target_velocity < 0 ? -1 : 0;
     
     if (detecting_ring) { // currently detecting ring
         this->current_ring_color = color_detected;
 
         if (!this->detected_ring) {
+            // should detect most of the way to the top, we want to go the next time that the ring passes the eject position
+            // assume that the hook is about 18.5 < (distance between things) links from the bottom
+            auto local_position = fmodf(position, this->links_per_hook); // chain local position
+            if (local_position < this->detect_position_ref - 8) {
+                local_position += this->links_per_hook;
+            }
+
+            this->bottom_position = position - local_position;
+            this->eject_position = this->bottom_position + this->eject_position_ref;
+            this->score_position = this->bottom_position + this->top_position_ref;
             if (this->detected_color() == *this->filter_mode) {
-                this->rotation_to_eject = this->intake_motor->position(rev) + 0.63;
                 this->ejection_pending = true;
             }
         }
-    } else if (this->detected_ring && !detecting_ring) { // ring just left detection
-        if (this->direction != -1) { // paused or upward
-            this->rotation_when_ring_lost = intake_motor->position(rev) + this->ejection_revs;
-        } else {
-            this->current_ring_color = NO_RINGS;
-        }
-    } else if (this->intake_motor->position(rev) > this->rotation_when_ring_lost) { // ring was scored
-        this->current_ring_color = NO_RINGS;
-        this->rotation_when_ring_lost = std::numeric_limits<float>::lowest();
     }
 
-    // reversing conditions
-    if (this->ejection_pending && this->intake_motor->position(rev) > this->rotation_to_eject && this->direction == 1 && this->intake_motor->velocity(rpm) > 0)
-        this->time_to_stop_reversing = vex::timer::system() + 333;
-    //else if (this->avg_current > 2.5)
-        //this->time_to_stop_reversing = vex::timer::system() + 333;
+    float ring_position = this->get_ring_position();
 
-    // spin
-    if (vex::timer::system() < this->time_to_stop_reversing) {
-        if (this->ejection_pending) {
-            this->ejection_pending = false;
-            this->ring_going_up = false;
-            this->current_ring_color = NO_RINGS;
-            this->rotation_when_ring_lost = std::numeric_limits<float>::lowest();
-        }
-        this->intake_motor->spin(forward, -12, volt);
-    } else {
-        this->ring_going_up = this->current_ring_color != NO_RINGS;
-        this->intake_motor->spin(forward, *this->target_velocity, volt);
+    // check if ring left the intake from the top or bottom
+    if (position > this->score_position || position < this->bottom_position) {
+        this->clear_ring();
     }
 
+    switch (this->state) {
+        case IntakeState::CALIBRATING:
+            if (detecting_ring) {
+                this->intake_motor->setPosition(this->detect_position_ref / this->links_per_rev, rev);
+                this->set_filter_mode_from_current_ring();
+                this->intake_motor->setVelocity(50, pct);
+                this->intake_motor->setStopping(hold);
+                this->intake_motor->spinTo(0, rev, true);
+                this->state = IntakeState::INPUT;
+            } else {
+                this->intake_motor->spin(forward, this->calibration_velocity_pct, percent);
+            }
+            break;        
+        case IntakeState::UNJAMMING:
+            this->intake_motor->spin(fwd, -100, pct);
+            if (ring_position > this->previous_ring_position + 0.5f * this->links_per_hook)
+                this->state = IntakeState::INPUT;
+            break;
+        default:
+            // reversing conditions
+            if (this->ejection_pending && position >= this->eject_position - (this->intake_motor->velocity(rpm) / 6000) * this->links_per_rev && this->direction == 1) {
+                this->reverse_end_time = vex::timer::system() + 188;
+                this->state = IntakeState::EJECTING;
+                this->clear_ring();
+            }
+
+            if (vex::timer::system() < this->reverse_end_time || (this->ejection_pending && this->direction == 1)) {
+                this->intake_motor->setStopping(brake);
+                this->intake_motor->spin(forward, 5.375 * (this->eject_position - position), volt);
+                this->velocity_error_buildup = 0.f;
+            } else {
+                this->intake_motor->spin(forward, *this->target_velocity, volt);
+
+                this->velocity_error_buildup = lerp<float>(
+                    this->velocity_error_buildup, 
+                    *this->target_velocity - rpm_to_volt(this->intake_motor->velocity(rpm), 
+                                                        get_rpm(*this->intake_motor)), 
+                                                        0.2);
+                // check if the intake has been stuck
+                if (fabsf(this->velocity_error_buildup) > 6) {
+                    this->state = IntakeState::UNJAMMING;
+                }
+            }
+            break;
+    }
+
+    this->previous_ring_position = ring_position;
     // update state
     this->detected_ring = detecting_ring;
-    this->direction = *this->target_velocity > 0 ? 1 : *this->target_velocity < 0 ? -1 : 0;
-/*
-    if ((vex::timer::system() / 10) % 10 == 1) {
-        std::cout 
-            //<< "direction = " << this->direction
-            << "filter = " << this->filter_mode
-            << ", reading = " << this->detected_color()
-            << ", color on intake = " << this->current_ring_color << std::endl;
-            //<< ", reverse time remaining = " << this->time_to_stop_reversing - vex::timer::system()
-            //<< ", current = " << this->intake_motor->current() << std::endl;
+
+    /*if (vex::timer::system() / 10 % 10 == 0) {
+        std::cout << ring_position << ", is calibrating = " << (this->state == IntakeState::CALIBRATING) << ", is unjamming = " << (this->state == IntakeState::UNJAMMING) << ", is ejecting = " << (vex::timer::system() < this->reverse_end_time) << ", filter = " << *this->filter_mode << std::endl;
     }*/
 }
 
 void Intake::intake() {
+    this->state = IntakeState::INPUT;
     *this->target_velocity = 12;
-    this->intake_motor->spin(forward, 12, volt);
 }
 
 void Intake::outtake() {
+    this->state = IntakeState::INPUT;
     *this->target_velocity = -12;
-    this->intake_motor->spin(forward, -12, volt);
 }
 
 void Intake::pickup() {
     this->intake_motor->setVelocity(100., pct);
-    this->intake_motor->spinFor(forward, 1.75, rev, false);
-};
-
-void Intake::update_color_detections() {
-    float red_reading = 0, blue_reading = 0, detection_reading = 0;
-    if (this->color_sensor->isNearObject()) {
-        auto reading = this->color_sensor->hue();
-        if (this->blue_hue.in_window(reading)) { // blue
-            detection_reading = 1.0f;
-            red_reading = 0.0f;
-            blue_reading = 1.0f;
-        } else if (this->red_hue.in_window(reading)) { // red
-            detection_reading = 1.0f;
-            red_reading = 1.0f;
-            blue_reading = 0.0f;
-        }
-    }
-
-    this->detection_confidence = lerp<float>(this->detection_confidence, detection_reading, this->new_reading_weight);
-    this->red_confidence = lerp<float>(this->red_confidence, red_reading, this->new_reading_weight);
-    this->blue_confidence = lerp<float>(this->blue_confidence, blue_reading, this->new_reading_weight);
+    this->intake_motor->spinTo(this->get_position() + this->links_per_hook / this->links_per_rev, rev, false);
 }
 
 bool Intake::sees_red() const {
@@ -125,20 +129,23 @@ bool Intake::sees_blue() const {
     return this->color_sensor->isNearObject() && this->blue_hue.in_window(this->color_sensor->hue());
 }
 
+float Intake::get_position() {
+    return this->intake_motor->position(rev) * this->links_per_rev; 
+}
+
 RingColor Intake::detected_color() const {
-    if (this->sees_red()) return RED_RINGS;
-    if (this->sees_blue()) return BLUE_RINGS;
+    if (!this->color_sensor->isNearObject()) return NO_RINGS;
+    auto hue = this->color_sensor->hue();
+    if (this->red_hue.in_window(hue)) return RED_RINGS;
+    if (this->blue_hue.in_window(hue)) return BLUE_RINGS;
     return NO_RINGS;
 }
 
 void Intake::clear_ring() {
     this->current_ring_color = NO_RINGS;
-    this->rotation_when_ring_lost = std::numeric_limits<float>::lowest();
-    this->ring_going_up = false;
-}
-
-void Intake::on_jam(void (*callback)()) {
-    this->on_jam_callback = callback;
+    this->ejection_pending = false;
+    if (this->state == IntakeState::EJECTING)
+        this->state = IntakeState::INPUT;
 }
 
 void Intake::raise() {
@@ -153,24 +160,20 @@ void Intake::set_raised(bool is_raised) {
     this->intake_piston->set(is_raised);
 }
 
-bool Intake::has_ring_at_top() const {
-    return this->detected_ring;
-}
-
 RingColor Intake::get_current_ring_color() const {
     return this->current_ring_color;
 }
 
-bool Intake::has_ring_going_up() const {
-    return this->ring_going_up;
+bool Intake::has_ring() const {
+    return this->current_ring_color != NO_RINGS;
 }
 
 void Intake::set_filter_mode(RingColor color) {
     *this->filter_mode = color;
 }
 
-void Intake::set_filter_mode_current() {
-    *this->filter_mode = this->current_ring_color == RED_RINGS ? BLUE_RINGS : this->current_ring_color == BLUE_RINGS ? RED_RINGS : ALL_RINGS;
+void Intake::set_filter_mode_from_current_ring() {
+    *this->filter_mode = this->current_ring_color == RED_RINGS ? BLUE_RINGS : this->current_ring_color == BLUE_RINGS ? RED_RINGS : NO_RINGS;
 }
 
 bool Intake::is_spinning() const {
@@ -179,9 +182,25 @@ bool Intake::is_spinning() const {
 
 void Intake::off() {
     this->target_velocity = 0;
+    this->state = IntakeState::INPUT;
     this->intake_motor->spin(fwd, 0, volt);
 }
 
 void Intake::set_velocity(float velocity) {
+    if (this->state == IntakeState::CALIBRATING && velocity == 0)
+        return;
+    this->state = IntakeState::INPUT;
     *this->target_velocity = velocity;
+}
+
+void Intake::calibrate() {
+    this->state = IntakeState::CALIBRATING;
+}
+
+float Intake::get_ring_position() {
+    auto position = this->get_position();
+    if (this->current_ring_color != NO_RINGS)
+        return position - this->bottom_position;
+    else
+        return fmod(position, this->links_per_hook);
 }
